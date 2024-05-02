0c4be00468ec8c7b97eb50a8d99f4e84cf471fc7
Sibeko Siphiwe <143623793+CyberGhost808@users.noreply.github.com>
2024-05-02 23:43:13 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,5 @@
+let HomeProuctImage = document.getElementById("HomeProuctImage");
+let HomeProuctName = document.getElementById("HomeProuctName");
+let HomeProuctPrice = document.getElementById("HomeProuctPrice");
+
+const product = ["asdas", "asdas"] 
